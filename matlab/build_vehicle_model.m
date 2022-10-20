34b94ad4aee4fee913bbd06299ebf566f2a7fb8d
Anton Pozharskiy <apozharski@gmail.com>
2022-10-20 09:51:49 +0200

    working model without tire forces, added velocity visualization

--- matlab/build_vehicle_model.m
+++ matlab/build_vehicle_model.m
@@ -6,38 +6,57 @@ if nargin == 1
     unfold_struct(varargin{1},'caller')
 end
 %% Variable
-v_wheel = MX.sym('v_wheel');
+t_brake = MX.sym('t_brake');
+j_brake = MX.sym('j_brake');
+t_engine = MX.sym('t_engine');
+j_engine = MX.sym('j_engine');
+omega_steer = MX.sym('omega_steer');
+u = MX.sym('u');
+v = MX.sym('v');
+omega = MX.sym('omega');
 delta = MX.sym('delta');
 phi = MX.sym('phi');
 %% constants
-L   = 1;
-l_r = 0.5;
+L_r = 0.5;
+L_f = 0.5;
+L = L_r+L_f;
+k_engine = 1;
+k_brake = 1;
+
 %% vehicle model ODEs
-beta = atan2(l_r * tan(delta), L);
-u = v_wheel * cos(beta);
-v = v_wheel * sin(beta);
-omega =  v_wheel * tan(delta) * cos(beta) / L;
-delta_dot = phi; % steering rate
+beta = atan2(L_r * tan(delta), L);
+slip_f = delta - (v-L_f*omega)/u;
+slip_r = -(v-L_r*omega)/u;
+
+t_brake_dot = j_brake;
+t_engine_dot = j_engine;
+delta_dot = omega_steer; % steering rate
+u_dot = (k_engine*t_engine-k_brake*t_brake)*cos(beta);
+v_dot = (k_engine*t_engine-k_brake*t_brake)*sin(beta);
+omega_dot = (k_engine*t_engine-k_brake*t_brake)*tan(delta)*cos(beta)/L;
 
 
-u_veh = [v_wheel;phi];
-x_veh = [delta];
-f_veh = [phi];
+u_veh = [j_brake;j_engine;omega_steer];
+x_veh = [u;v;omega;t_brake;t_engine;delta];
+f_veh = [u_dot;v_dot;omega_dot;t_brake_dot;t_engine_dot;delta_dot];
 
-Jbx_veh = [1];
-lbx_veh = [-1];
-ubx_veh = [1];
+Jbx_veh = [0,0,0,0,0,1
+           1,0,0,0,0,0
+           0,0,0,1,0,0
+           0,0,0,0,1,0];
+lbx_veh = [-1;0;0;0];
+ubx_veh = [1;5;1;1];
 
-Jbu_veh = eye(2);
-lbu_veh = [0;-0.1];
-ubu_veh = [1;0.1];
+Jbu_veh = eye(3);
+lbu_veh = [-1;-1;-0.5];
+ubu_veh = [1;1;0.5];
 
 Jbx_e_veh = [];
 lbx_e_veh = [];
 ubx_e_veh = [];
 
-Jbx0_veh = [1];
-x0_veh = [0];
+Jbx0_veh = eye(6);
+x0_veh = zeros(6,1);
 %% Generic part
 % (make local workspace a struct and pass to output
 names = who;
--- matlab/plot_solution.m
+++ matlab/plot_solution.m
@@ -1,14 +1,32 @@
-function plot_solution(r_x,r_y,r_theta,r_s,v_s,v_n)
+function plot_solution(r_x,r_y,r_theta,r_s,v_traj)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
+
+% Break out vehicle 
+v_s = v_traj(1,:);
+v_n = v_traj(2,:);
+v_alpha = v_traj(3,:);
+v_u = v_traj(5,:);
+v_v = v_traj(6,:); 
+v_delta = v_traj(10,:);
+
+% Calculate left and right hand side
 r_lx = r_x - sin(r_theta);
 r_ly = r_y + cos(r_theta);
 r_rx = r_x + sin(r_theta);
 r_ry = r_y - cos(r_theta);
 
+% Calculate vehicle poses
+v_s(end) = 25; %hack to fix nans
 v_theta = interp1(r_s,r_theta,v_s);
 v_x = interp1(r_s,r_x,v_s) - v_n.*sin(v_theta);
 v_y = interp1(r_s,r_y,v_s) + v_n.*cos(v_theta);
+
+% Calculate velocities
+rotation = exp((v_theta+v_alpha)*1i);
+v_uv_rotated = rotation .* ([1 1i]*[v_u;v_v]);
+v_u_rotated = real(v_uv_rotated);
+v_v_rotated = imag(v_uv_rotated);
 
 figure;
 hold on;
@@ -17,5 +35,5 @@ plot(r_lx,r_ly, 'r');
 plot(r_rx,r_ry, 'r');
 plot(v_x,v_y,'g');
 axis equal;
-
+quiver(v_x,v_y,v_u_rotated,v_v_rotated);
 end
