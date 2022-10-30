5efc9a9c01709a1612fe47968f74e7177156a66e
Anton Pozharskiy <apozharski@gmail.com>
2022-10-30 14:45:12 +0100

    poorly working

--- matlab/mpc_car_sim.m
+++ matlab/mpc_car_sim.m
@@ -12,15 +12,15 @@ nu = length(model.sym_u);
 
 %% arguments
 compile_interface = 'auto';
-method = 'erk'; % irk, irk_gnsf
+method = 'irk'; % irk, irk_gnsf
 model_name =  'sim_car';
 
 % simulation parameters
-N_sim = 100;
+N_sim = 1000;
 h = 1; % simulation time
-x0 = [0; 0; 0; 0.1; 0; 0; 0; 0; 0; 0]; % initial state
+x0 = [0; 0; 0; 0.01; 5; 0; 0; 0; 0; 0.0]; % initial state
 %x0 = [0; 0; 0;xtraj(4,1);0]; % initial state
-u0 = [1,0.01;]; % control input
+u0 = [1,0.01;]; % control inputs
 
 %% acados sim model
 sim_model = acados_sim_model();
@@ -28,10 +28,11 @@ sim_model.set('name', model_name);
 sim_model.set('T', h);
 
 sim_model.set('sym_x', model.sym_x);
+sim_model.set('sym_xdot',model.sym_xdot)
 sim_model.set('sym_u', model.sym_u);
 
-sim_model.set('dyn_type', 'explicit');
-sim_model.set('dyn_expr_f', model.expr_f_expl);
+sim_model.set('dyn_type', 'implicit');
+sim_model.set('dyn_expr_f', model.expr_f_impl);
 
 %% acados sim opts
 sim_opts = acados_sim_opts();
@@ -39,7 +40,7 @@ sim_opts = acados_sim_opts();
 sim_opts.set('compile_interface', compile_interface);
 sim_opts.set('num_stages', 2);
 sim_opts.set('num_steps', 3);
-sim_opts.set('newton_iter', 2); % for implicit intgrators
+sim_opts.set('newton_iter', 3); % for implicit intgrators
 sim_opts.set('method', method);
 
 
@@ -53,9 +54,12 @@ x_sim(:,1) = x0;
 u_sim = zeros(nu, N_sim);
 t = 0;
 dt = x_sim(4,1);
-j_engine = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0.5,0,-0.5,0,0,0,0]);
-j_brake = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0.5,0,-.5,0,0]);
-omega_steer = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0.1,0.1,0,-0.1,-0.1,0,0]);
+j_engine = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0,0,0,0,0]);
+%j_engine = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0,0,0,0,0]);
+j_brake = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0,0,0,0,0]);
+%j_brake = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0,0,0,0,0]);
+omega_steer = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0.1,0,-0.1,-0.1,0,0,0]);
+%omega_steer = interpolant('j_engine','linear',{[0,1,2,3,4,5,6,7]},[0,0,0,0,0,0,0,0]);
 for ii=1:N_sim
 	% set initial state
 	sim.set('x', x_sim(:,ii));
@@ -73,7 +77,6 @@ for ii=1:N_sim
 
 	% solve
 	sim.solve();
-
 	% get simulated state
 	x_sim(:,ii+1) = sim.get('xn');
     t = t+dt;
